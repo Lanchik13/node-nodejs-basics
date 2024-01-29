57380d569b966ac12112cb982b48b237cdf84334
Svetlana Plinda <sv.plinda@icloud.com>
2024-01-29 23:36:33 +0100

    feat: solve task read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import url from "node:url";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const _fileName = url.fileURLToPath(import.meta.url);
+  const currentDirectory = path.dirname(_fileName);
+  const file2Read = path.join(currentDirectory, "files", "fileToRead.txt");
+
+  try {
+    await fs.promises
+    .access(file2Read)
+    .catch(() => {
+      throw new Error("There is no fileToRead.txt");
+    });
+
+    const content = await fs.promises.readFile(file2Read, "utf8");
+    console.log(content);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await read();
