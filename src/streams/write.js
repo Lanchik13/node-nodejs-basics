51555694c72727b5426461ae80b147e00699fd12
Svetlana Plinda <sv.plinda@icloud.com>
2024-01-29 23:48:45 +0100

    feat: solve tasks streams read and write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import url from "node:url";
+import path from "path";
+
+const { stdin } = process;
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const _fileName = url.fileURLToPath(import.meta.url);
+    const currentDirectory = path.dirname(_fileName);
+    const file2Write = path.join(currentDirectory, "files", "fileToWrite.txt");
+
+    const writeStream = fs.createWriteStream(file2Write);
+
+    stdin.on('data', (data) => {
+        writeStream.write(data);
+      });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await write();
