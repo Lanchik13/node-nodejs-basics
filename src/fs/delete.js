1206704d8edcb074d981c37c2035d29fec96af20
Svetlana Plinda <sv.plinda@icloud.com>
2024-01-29 23:23:56 +0100

    feat: solve task delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import url from "node:url";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const _fileName = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(_fileName);
+  const file2Remove = path.join(dirname, "files", "fileToRemove.txt");
+
+  try {
+    await fs.promises
+      .access(file2Remove)
+      .then(() => {})
+      .catch(() => {
+        throw new Error("File does not exist!");
+      });
+
+    await fs.promises.unlink(file2Remove);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await remove();
