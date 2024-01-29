9ae8fa7b17fcb2e6b75eee7a2382cd2432c963f0
Svetlana Plinda <sv.plinda@icloud.com>
2024-01-29 23:31:13 +0100

    feat: solve task list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import url from "node:url";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  const _fileName = url.fileURLToPath(import.meta.url);
+  const currentDirectory = path.dirname(_fileName);
+
+  const sourceDirectory = path.join(currentDirectory, "files");
+
+  try {
+    await fs.promises.access(sourceDirectory).catch(() => {
+      throw new Error("Directory does not exist!");
+    });
+    const filesInSrcDir = await fs.promises.readdir(sourceDirectory);
+    filesInSrcDir.forEach((fileName) => {
+      console.log(fileName);
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await list();
